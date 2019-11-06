720da7075a10b0785ede8266afd93b8c1987c4f4
maximdervis <maximdervis@gmail.com>
2019-11-06 22:40:40 +0300

    Updates

---
+++ precache-manifest.5dd6f12b422adac626a52b5986c701b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bff2970707a958bb1cd6cbc94eb20b5d",
+    "url": "/friends-list/index.html"
+  },
+  {
+    "revision": "69690824474b172e95c2",
+    "url": "/friends-list/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "2c0aab94df201fe18d9b",
+    "url": "/friends-list/static/css/main.bc409b70.chunk.css"
+  },
+  {
+    "revision": "69690824474b172e95c2",
+    "url": "/friends-list/static/js/2.3267049e.chunk.js"
+  },
+  {
+    "revision": "2c0aab94df201fe18d9b",
+    "url": "/friends-list/static/js/main.fb50dd08.chunk.js"
+  },
+  {
+    "revision": "df9c9a8f8bfdf67ee383",
+    "url": "/friends-list/static/js/runtime-main.9b3b1eab.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/friends-list/static/media/SEGA.12a3db8b.woff"
+  }
+]);
